7f5e831f5767c29f8fab47046249343de60192fb
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-11-18 14:11:37 +0000

    adds hover scale effect to buttons

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     heroImage.style.transition = "opacity 2s";
     heroImage.style.opacity = 1;
   }, 100);
+
+ 
+  const buttons = document.querySelectorAll(".indexbutton1, .indexbutton2, .button2");
+  buttons.forEach(btn => {
+    btn.addEventListener("mouseover", () => {
+      btn.style.transform = "scale(1.05)";
+      btn.style.transition = "transform 0.3s";
+    });
+    btn.addEventListener("mouseout", () => {
+      btn.style.transform = "scale(1)";
+    });
+  });
+
